a81786b915f3d70889a6c14fe8e6cee9eca466e9
Aleksandr Belousov <aleksandr.b@smplt.net>
2022-07-08 17:03:32 +0300

    some fixes

--- src/Service.BrokerFeeApplier/Modules/ServiceBusModule.cs
+++ src/Service.BrokerFeeApplier/Modules/ServiceBusModule.cs
@@ -14,16 +14,9 @@ namespace Service.BrokerFeeApplier.Modules
     {
         protected override void Load(ContainerBuilder builder)
         {
-            var serviceBusLogger = Program.LogFactory.CreateLogger(nameof(MyServiceBusTcpClient));
-
             var serviceBusClient = builder.RegisterMyServiceBusTcpClient(
                 () => Program.Settings.SpotServiceBusHostPort,
                 Program.LogFactory);
-            serviceBusClient.Log.AddLogException(ex => serviceBusLogger.LogInformation(ex, "Exception in MyServiceBusTcpClient"));
-            serviceBusClient.Log.AddLogInfo(info => serviceBusLogger.LogDebug($"MyServiceBusTcpClient[info]: {info}"));
-            serviceBusClient.SocketLogs.AddLogInfo((context, msg) => serviceBusLogger.LogInformation($"MyServiceBusTcpClient[Socket {context?.Id}|{context?.ContextName}|{context?.Inited}][Info] {msg}"));
-            serviceBusClient.SocketLogs.AddLogException((context, exception) => serviceBusLogger.LogInformation(exception, $"MyServiceBusTcpClient[Socket {context?.Id}|{context?.ContextName}|{context?.Inited}][Exception] {exception.Message}"));
-            builder.RegisterInstance(serviceBusClient).AsSelf().SingleInstance();
 
             builder.RegisterType<FeeApplierJob>().AsSelf().SingleInstance().AutoActivate();
 
